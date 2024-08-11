8c7bfa570891ca3e1ec020229d840b339cf41817
Thuong To <thuongthanhto@gmail.com>
2024-08-11 22:49:36 +0700

    Update config

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,6 @@
-const config = require("../config/db.config.js");
-const Sequelize = require("sequelize");
+const config = require('../config/db.config.js')
+const Sequelize = require('sequelize')
+const mysql2 = require('mysql2')
 
 const sequelize = new Sequelize(
   config.db_name,
@@ -9,31 +10,35 @@ const sequelize = new Sequelize(
     host: config.db_host,
     port: config.db_port,
     dialect: config.db_dialect,
-    dialectModule: "mysql2",
+    dialectModule: mysql2,
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
     pool: {
       max: config.db_pool.max,
       min: config.db_pool.min,
       acquire: config.db_pool.acquire,
-      idle: config.db_pool.idle
-    }
+      idle: config.db_pool.idle,
+    },
   }
-);
+)
 
-const db = {};
+const db = {}
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+db.Sequelize = Sequelize
+db.sequelize = sequelize
 
-db.user = require("./user.model.js")(sequelize, Sequelize);
-db.role = require("./role.model.js")(sequelize, Sequelize);
+db.user = require('./user.model.js')(sequelize, Sequelize)
+db.role = require('./role.model.js')(sequelize, Sequelize)
 
 db.role.belongsToMany(db.user, {
-  through: "user_roles"
-});
+  through: 'user_roles',
+})
 db.user.belongsToMany(db.role, {
-  through: "user_roles"
-});
+  through: 'user_roles',
+})
 
-db.ROLES = ["user", "admin", "moderator"];
+db.ROLES = ['user', 'admin', 'moderator']
 
-module.exports = db;
+module.exports = db
