b50d00c17497378e9c72a16d485c2f95368c9e06
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-08-11 12:47:32 +0700

    edit env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
   db_host: process.env.DB_HOST,
   db_user: process.env.DB_USER,
@@ -6,9 +7,9 @@ module.exports = {
   db_dialect: process.env.DB_DIALECT,
   db_port: process.env.DB_PORT,
   db_pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    max: parseInt(process.env.DB_POOL_MAX, 10) || 5,
+    min: parseInt(process.env.DB_POOL_MIN, 10) || 0,
+    acquire: parseInt(process.env.DB_POOL_ACQUIRE, 10) || 30000,
+    idle: parseInt(process.env.DB_POOL_IDLE, 10) || 10000
   }
 };
