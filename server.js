4aad18f46c650af5520a5bbd0f67acff97e10361
Thuong To <thuongthanhto@gmail.com>
2024-08-11 22:20:19 +0700

    Update config

--- server.js
+++ server.js
@@ -55,8 +55,4 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+module.exports = app;
