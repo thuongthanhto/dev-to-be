71b1286c756153d3dafe5fb29acf9d1f8c5f4524
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-08-11 12:59:48 +0700

    edit index.js model

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,7 +4,7 @@ module.exports = {
   db_user: process.env.DB_USER,
   db_password: process.env.DB_PASSWORD,
   db_name: process.env.DB_NAME,
-  db_dialect: process.env.DB_DIALECT,
+  db_dialect: process.env.DB_DIALECT || 'mysql',
   db_port: process.env.DB_PORT,
   db_pool: {
     max: parseInt(process.env.DB_POOL_MAX, 10) || 5,
